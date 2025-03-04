51b327cc124188bbbe2c5957ce651234e815d91c
{JosephNgugi254} <{solitaryjoe069@gmail.com}>
2025-03-04 11:39:43 +0300

    added functions

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+function writeCards(myNames, event) {
+    let newMyNames = [];
+    for (let i = 0; i<myNames.length; i++) {
+        newMyNames.push(`Thank you, ${myNames[i]}, for the wonderful ${event} gift!`);
+    }
+    return newMyNames;
+}
+
+writeCards(["Charlie", "Samip", "Ali"], "birthday");
+
+function countDown(num) {
+    let i = num;
+    while (i >= 0) {
+        console.log(i);
+        i--;
+    } 
+}
+
+countDown(12);
